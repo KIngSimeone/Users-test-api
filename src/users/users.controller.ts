0fed7e3edf47d0f10241365daf33f2f82a967d22
Simeon Umoh <simeoneumoh@outlook.com>
2024-03-12 12:22:04 +0100

    clean

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,13 @@
 // users.controller.ts
-import { Controller, Get, Post, Param, Body, Put, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -11,28 +19,48 @@ export class UsersController {
     return this.usersService.findAll();
   }
 
-  @Get(':id')
-  async findById(@Param('id') id: string) {
-    return this.usersService.findById(+id); // Convert id to a number
+  @Get('/get-user/:id')
+  async findById(@Param('id') id: number) {
+    return this.usersService.findById(id);
   }
 
-  @Post()
-  async create(@Body() createUser: { firstName: string; lastName: string; email: string; phoneNumber: string }) {
+  @Post('/add-user')
+  async create(
+    @Body()
+    createUser: {
+      firstName: string;
+      lastName: string;
+      email: string;
+      phoneNumber: string;
+    },
+  ) {
     const { firstName, lastName, email, phoneNumber } = createUser;
     return this.usersService.create(firstName, lastName, email, phoneNumber);
   }
 
-  @Put(':id')
+  @Put('/update-user/:id')
   async update(
-    @Param('id') id: string,
-    @Body() updateUser: { firstName: string; lastName: string; email: string; phoneNumber: string },
+    @Param('id') id: number,
+    @Body()
+    updateUser: {
+      firstName: string;
+      lastName: string;
+      email: string;
+      phoneNumber: string;
+    },
   ) {
     const { firstName, lastName, email, phoneNumber } = updateUser;
-    return this.usersService.update(+id, firstName, lastName, email, phoneNumber); // Convert id to a number
+    return this.usersService.update(
+      id,
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+    );
   }
 
-  @Delete(':id')
-  async remove(@Param('id') id: string) {
-    return this.usersService.remove(+id); // Convert id to a number
+  @Delete('/delete-user/:id')
+  async remove(@Param('id') id: number) {
+    return this.usersService.remove(id);
   }
 }
