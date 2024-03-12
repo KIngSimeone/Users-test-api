c63c2528480a9f704c0ed78afb1a1593209d912d
Simeon Umoh <simeoneumoh@outlook.com>
2024-03-12 11:53:34 +0100

    setup user model

--- src/sequelize.config.ts
+++ src/sequelize.config.ts
@@ -1,15 +1,14 @@
-// sequelize.config.ts
 import { SequelizeModuleOptions } from '@nestjs/sequelize';
 
 const sequelizeConfig: SequelizeModuleOptions = {
   dialect: 'mysql',
   host: process.env.DB_HOST,
-  port: 3306,
+  port: 17241,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  autoLoadModels: true, // automatically import models
-  synchronize: true, // sync models with the database (only in development)
+  autoLoadModels: true,
+  synchronize: true,
 };
 
 export default sequelizeConfig;
---
+++ src/users/users.schema.ts
@@ -0,0 +1,16 @@
+import { Model, Column, Table } from 'sequelize-typescript';
+
+@Table
+export class User extends Model {
+  @Column
+  firstName: string;
+
+  @Column
+  lastName: string;
+
+  @Column
+  email: string;
+
+  @Column
+  phoneNumber: string;
+}
